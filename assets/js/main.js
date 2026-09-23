cae1a59a61c135c56699967a7384dc7f138f232e
Liam <liporter@davidson.edu>
2026-09-23 10:34:08 -0400

    JS update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,3 @@
-
+console.log("My intentions around this piece were to highlight the controversy of user privacy among Meta Services");
+console.log("I focused on the most popular platforms, Facebook. When hovering over the create account, an eye appears where the Facebook logo was");
+console.log("This signifies that Meta is watching you and your acccount data when you create an account.");
